9995ca19c6da314263b7692f09b98e809a78eea7
Klajdi Nushi <cloodee@hotmail.com>
2019-09-01 11:30:22 +0300

    MenuItem Home View

--- FoodDelivery/Controllers/Customer/HomeController.cs
+++ FoodDelivery/Controllers/Customer/HomeController.cs
@@ -5,14 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodDelivery.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+            };
+
+            return View(IndexVM);
         }
         
         public IActionResult Privacy()
---
+++ FoodDelivery/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItems> MenuItems { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Coupon> Coupon { get; set; }
+    }
+}
